19412696a3fe025ec60d934794e3e9d74bd105fc
SakthiSri15052001 <102963114+SakthiSri15052001@users.noreply.github.com>
2022-04-06 11:53:50 +0530

    change on 6th line by sakthi branch

--- src/com/sakthi/samplepro.java
+++ src/com/sakthi/samplepro.java
@@ -3,7 +3,7 @@ package com.sakthi;
 public class samplepro {
 	public static void display()
 	{
-		System.out.println("sakthi");
+		System.out.println("sakthisri");
 	}
 	public static void main(String[] args) {
 		
